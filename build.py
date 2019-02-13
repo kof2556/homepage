e263062964055580bf18b510e3cc6c2402d0f14f
kof2556 <kof2556@yahoo.com>
2019-02-12 22:37:32 -0800

    added another branch with python script

---
+++ build.py
@@ -0,0 +1,17 @@
+
+hey = open('templates/top.html').read()
+there = open('content/index.html').read()
+gorgeous = open('templates/bottom.html').read()
+
+print(hey + there + gorgeous)
+
+
+
+
+
+
+
+
+
+
+
