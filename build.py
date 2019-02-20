706bb2412b545b31be63736baa491bb102da070d
kof2556 <kof2556@yahoo.com>
2019-02-20 15:42:09 -0800

    modified script and created hbase file

--- build.py
+++ build.py
@@ -1,11 +1,45 @@
 
-
+pages = []
 def main():
-   top = open('templates/top.html').read()
-   center = open('content/index.html').read()
-   bottom = open('templates/bottom.html').read()
-   print(top + center + bottom)
-   
+    top = open('templates/top.html').read()
+    center = open('content/index.html').read()
+    bottom = open('templates/bottom.html').read()
+    print(top + center + bottom)
 
+
+
+
+pages = [
+     {
+        "filename": "content/about.html",
+        "output": "docs/about.html",  
+        "title": "About Me", 
+    },
+    {
+        "filename": "content/projects.html",
+        "output": "docs/about.html",  
+        "title": "Projects", 
+    },
+    {
+        "filename": "content/blog.html",
+        "output": "docs/about.html",  
+        "title": "Blog", 
+    },
+    {
+        "filename": "content/index.html",
+        "output": "docs/about.html",  
+        "title": "Index", 
+    }         
+]
+for page in pages:
+    page_title = page['title']
+    print(page_title)
+   
 if __name__ == "__main__":
-    main()
+    main()   
+  
+
+
+ 
+
+
