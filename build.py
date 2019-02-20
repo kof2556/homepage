8988fbae8987f628a6348f64103aec10aa6e41d7
kof2556 <kof2556@yahoo.com>
2019-02-19 20:38:56 -0800

    some deletes and refactorin

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 
-nola = open('templates/top.html').read()
-saints = open('content/index.html').read()
-pelicans = open('templates/bottom.html').read()
 
-print(nola + saints + pelicans)
+def main():
+   top = open('templates/top.html').read()
+   center = open('content/index.html').read()
+   bottom = open('templates/bottom.html').read()
+   print(top + center + bottom)
+   
 
+if __name__ == "__main__":
+    main()
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Scrip to combine three files
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-
