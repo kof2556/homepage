baab7a92f62bd3ea13eb52012fe062c00a5ad69b
kof2556 <kof2556@yahoo.com>
2019-02-22 18:12:36 -0800

    reorganizing top and bottom htmls

--- build.py
+++ build.py
@@ -1,45 +1,49 @@
 
-pages = []
 def main():
-    top = open('templates/top.html').read()
-    center = open('content/index.html').read()
-    bottom = open('templates/bottom.html').read()
-    print(top + center + bottom)
+    #template = open("base.html").read()
+    #index_content = open("content/index.html").read()
+   #finished_index_page = template.replace("{{content}}", index_content)
+   #open("docs/index.html", "w+").write(finished_index_page)
+    #top = open('templates/top.html').read()
+    #center = open('content/index.html').read()
+    #bottom = open('templates/bottom.html').read()
+    #print(top + center + bottom)
 
 
 
 
-pages = [
-     {
+    pages = [
+        {
         "filename": "content/about.html",
         "output": "docs/about.html",  
         "title": "About Me", 
-    },
-    {
+        },
+        {
         "filename": "content/projects.html",
-        "output": "docs/about.html",  
+        "output": "docs/project.html",  
         "title": "Projects", 
-    },
-    {
+        },
+        {
         "filename": "content/blog.html",
-        "output": "docs/about.html",  
+        "output": "docs/blog.html",  
         "title": "Blog", 
-    },
-    {
-        "filename": "content/index.html",
-        "output": "docs/about.html",  
+        },
+        {
+        "filename": "content/first.html",
+        "output": "docs/index.html",  
         "title": "Index", 
-    }         
-]
-for page in pages:
-    page_title = page['title']
-    print(page_title)
+        },         
+    ]
+    for page in pages:
+        template = open("templates/base.html").read()
+        content = open(page["filename"]).read()
+        finished_page = template.replace("{{content}}", content)
+        open(page["output"], "w+").write(finished_page)
+        #page_title = page['title']
+        #print(page_title)
+        print('done')
    
 if __name__ == "__main__":
     main()   
   
-
-
- 
-
 
