2ac343d56ecdd39b1f03c720210c9610a8b5190e
kof2556 <kof2556@yahoo.com>
2019-02-26 23:23:18 -0800

    replaced list

--- build.py
+++ build.py
@@ -1,3 +1,6 @@
+
+
+
 def build_page(page_data):    
     template = open("templates/base.html").read()
     content = open(page_data["filename"]).read()
@@ -5,39 +8,38 @@ def build_page(page_data):
     
 def write_page(page, finished_page):
     open(page["output"], "w+").write(finished_page)
+    
 
 
 def main():
-   
+      
 
 
     pages = [
-        {
-        "filename": "content/about.html",
-        "output": "docs/about.html",  
-        "title": "About Me", 
-        },
-        {
-        "filename": "content/projects.html",
-        "output": "docs/project.html",  
-        "title": "Projects", 
-        },
-        {
-        "filename": "content/blog.html",
-        "output": "docs/blog.html",  
-        "title": "Blog", 
-        },
-        {
-        "filename": "content/first.html",
-        "output": "docs/index.html",  
-        "title": "Index", 
-        },         
+       
     ]
     for page in pages:
         finished_page = build_page(page)
         write_page(page, finished_page)
-        
-        print('done')
-   
+import glob
+all_html_files = glob.glob("content/*.html")
+print(all_html_files)
+
+import os
+
+file_path = "content/blog.html"
+file_name = os.path.basename(file_path)
+print(file_name)
+name_only, extension = os.path.splitext(file_name)
+print(name_only)
+
+pages = []
+pages.append({
+"filename": "content/index.html",
+"title": "Index",
+"output": "docs/index.html",
+})
+print(pages)
+         
 if __name__ == "__main__":
     main()   
